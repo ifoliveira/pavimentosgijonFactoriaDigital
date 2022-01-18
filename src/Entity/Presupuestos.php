f60d8f48878ecd67a1a1f0e03a766315dfdc545e
ifoliveira <ifoliveira@hotmail.com>
2022-01-18 12:20:35 +0100

    Update Presupuestos.php

--- src/Entity/Presupuestos.php
+++ src/Entity/Presupuestos.php
@@ -66,6 +66,12 @@ class Presupuestos
      * @ORM\Column(type="string", length=20)
      */
     private $tipopagosnalPe;
+    
+     /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $ticketsnal;
+
 
     /**
      * @ORM\OneToOne(targetEntity="App\Entity\Cestas", cascade={"persist", "remove"})
